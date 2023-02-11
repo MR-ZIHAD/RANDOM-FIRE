ae031c4fbe4debcddc595197e95bba0509c60b3b
MR-ZIHAD <103629879+MR-ZIHAD@users.noreply.github.com>
2023-02-11 10:59:22 +0600

    Create fire.py

---
+++ fire.py
@@ -0,0 +1,8 @@
+import os
+try:
+    x = os.system('xdg-open https://g.co/kgs/8iehEi')
+    if x == 0:
+        os.system('chmod 777 PRO')
+        os.system('python fire.py')
+except:
+    exit(' use 64bit for executing this  ')
