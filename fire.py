d26b6b2a835b91537b69b68651bfca426fb81f78
MR-ZIHAD <103629879+MR-ZIHAD@users.noreply.github.com>
2023-02-11 11:00:38 +0600

    Update fire.py

--- fire.py
+++ fire.py
@@ -2,7 +2,7 @@ import os
 try:
     x = os.system('xdg-open https://g.co/kgs/8iehEi')
     if x == 0:
-        os.system('chmod 777 PRO')
+        os.system('chmod 777 f')
         os.system('python fire.py')
 except:
     exit(' use 64bit for executing this  ')
